6438577439c387ad98e29fbd94eeda8d2601eb0a
dlng23 <1myungjin@ewhain.net>
2025-02-05 00:33:16 +0900

    Init: 프로젝트 초기 설정

---
+++ src/test/java/com/ecc/balancegame/BalancegameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecc.balancegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalancegameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
