c5abe5ba2803a4a330c38d8888aae43c5065b1ef
dlng23 <1myungjin@ewhain.net>
2025-02-11 19:29:32 +0900

    feat: 카테고리 목록 조회 기능 추가

---
+++ src/main/java/com/ecc/balancegame/domain/Category.java
@@ -0,0 +1,20 @@
+package com.ecc.balancegame.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @Column(nullable = false, name = "category_name")
+    private String categoryName;
+}
