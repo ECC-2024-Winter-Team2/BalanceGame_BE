0df2b8ac53823effa411f1da0a23ac843f4aa56e
dlng23 <1myungjin@ewhain.net>
2025-02-19 02:32:07 +0900

    chore: CORS 설정 변경

--- src/main/java/com/ecc/balancegame/config/CorsConfig.java
+++ src/main/java/com/ecc/balancegame/config/CorsConfig.java
@@ -13,10 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 엔드포인트 허용
-                        .allowedOrigins(
-                                "http://localhost:3003",
-                                "http://ec2-43-200-237-253.ap-northeast-2.compute.amazonaws.com"
-                        ) // 허용할 도메인 추가
+                        .allowedOrigins("http://43.200.237.253", "ec2-43-200-237-253.ap-northeast-2.compute.amazonaws.com") // 허용할 도메인 추가
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
