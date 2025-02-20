6aaf898c55d9752dc4c5257d4969f8e5d5202a41
dlng23 <1myungjin@ewhain.net>
2025-02-20 20:48:08 +0900

    fix: CORS 해결

--- src/main/java/com/ecc/balancegame/config/WebConfig.java
+++ src/main/java/com/ecc/balancegame/config/WebConfig.java
@@ -1,21 +1,17 @@
 package com.ecc.balancegame.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
-                .addMapping("/*")
-                .allowedHeaders("")
-                .allowedOriginPatterns("*")
-        ;
+                .addMapping("/**")
+                .allowedOrigins("*")  // 모든 origin 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")  // 허용할 HTTP 메소드
+                .allowedHeaders("*");  // 모든 헤더 허용
     }
-
 }
